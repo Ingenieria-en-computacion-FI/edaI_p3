9b24aab733e7c914f9bf6ca932b7604f9196f3f7
adaramm <ariel.mercado@ingenieria.unam.edu>
2026-03-05 21:50:32 -0600

    Change include guards in nodo.h
    Updated include guards in nodo.h for consistency.

--- nodo/nodo.h
+++ nodo/nodo.h
@@ -1,10 +1,9 @@
-#ifndef NODO_H
-#define NODO_H
+#ifndef __NODO_H__
+#define __NODO_H__
 
 /* Declaración incompleta del tipo Nodo (tipo opaco) */
 typedef struct Nodo Nodo;
 
-// TODO: Definir la estructura Nodo que debe contener un entero llamado valor
 
 /* Crea un nodo con el valor indicado */
 Nodo* crearNodo(int valor);
